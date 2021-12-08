46960e84f80aa38a4d7d0e225b59b591a68bc75c
gadiazsaavedra <gadiazsaavedra@gmail.com>
2021-12-08 16:30:26 -0300

    con DB

---
+++ ProyectoFamiliar/.history/ProyectoFamiliar/AppFamiliar/models_20211208143825.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Padres(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    parentesco = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+    
+class Hijos(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    
+class Con
